5475a1b5d46945b556ecf995f27d7039b6da5a6f
Deepanchakaravarthy7237 <35687584+Deepanchakaravarthy7237@users.noreply.github.com>
2018-02-05 13:48:42 +0530

    Update 11.c

--- 11.c
+++ 11.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
+#include<conio.h>
 int main()
 {
     int low, high, i, flag;
+    clrscr();
     printf("Enter two numbers(intervals): ");
     scanf("%d %d", &low, &high);
-
-    printf("Prime numbers between %d and %d are: ", low, high);
-
-    while (low < high)
+  printf("Prime numbers between %d and %d are: ", low, high);
+ while (low < high)
     {
         flag = 0;
-
-        for(i = 2; i <= low/2; ++i)
+for(i = 2; i <= low/2; ++i)
         {
             if(low % i == 0)
             {
@@ -22,8 +21,7 @@ int main()
 
         if (flag == 0)
             printf("%d ", low);
-
-        ++low;
+ ++low;
     }
 
     return 0;
