a0b475ba03e9f689ccb166295fc13833e6fa971a
Deepanchakaravarthy7237 <35687584+Deepanchakaravarthy7237@users.noreply.github.com>
2018-02-19 14:14:13 +0530

    Create 113.c

---
+++ 113.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int f1=0,f2=1,f3,n,i;
+	printf("Enter the Number of Input:");
+	scanf("%d",&n);
+	for(i=1;i<=n;++i)
+	{
+		f3=f1+f2;
+		f1=f2;
+		f2=f3;
+		printf("%d",f1);
+	}
+	
+}
