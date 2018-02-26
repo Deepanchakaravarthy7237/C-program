4b55f7ffcfb4fa4f72f3a0273f1dc6af12d1e031
Deepanchakaravarthy7237 <35687584+Deepanchakaravarthy7237@users.noreply.github.com>
2018-02-26 13:35:07 +0530

    Create 1414.c

---
+++ 1414.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a=1,b=1,i,n,c;
+    clrscr();
+    printf("\n enter fabi number");
+{
+    scanf("%d",&n);
+    printf("\n value a");
+    printf("\n value b");
+    (i=1,i=n-2,i++)
+}
+{
+c=a+b;
+printf("\n tne value of c")
+}
+getch();
+}
