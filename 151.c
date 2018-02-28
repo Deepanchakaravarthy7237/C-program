91af56be60d17bcc092ff92281c83180dad6662c
Deepanchakaravarthy7237 <35687584+Deepanchakaravarthy7237@users.noreply.github.com>
2018-02-28 14:18:53 +0530

    Create 151.c

---
+++ 151.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<conio.h>
+int main(void) 
+{
+	char str[50];
+	int i;
+  clrscr();
+	printf("enter the string:");
+	scanf("%[^\n]s",str);
+	if(printf("\n%s",str))
+	{
+		
+	}
+	return 0;
+}
